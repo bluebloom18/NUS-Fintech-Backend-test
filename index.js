62c59fb9f09dbf751dd0613c1ef6af6bf79ee7b3
Lien Ber Luen <lienbl@yahoo.com>
2021-12-10 23:43:44 +0800

    added API

--- index.js
+++ index.js
@@ -10,8 +10,38 @@ app.use(bodyParser.json());
 
 app.use('/', usersRoutes);
 
+let hometext = `Hello from homepage.
+<h1>
+APIs are:
+</h1>
+<h2>GET:</h2>
+<ul>
+<li>/users/all</li>
+<li>/users/:id</li>
+<li>/users/?age=20</li>
+<li>/transactions/all</li>
+<li>/transactions/:id</li>
+</ul>
+
+<h2>CREATE:</h2>
+<ul>
+<li>/users/ </li>
+</ul>
+
+<h2>DELETE:</h2>
+<ul>
+<li>/users/:id </li>
+</ul>
+
+<h2>UPDATE:</h2>
+<ul>
+<li>/users/:id </li>
+</ul>
+
+`
+
 app.get('/', (req,res)=>{
-    res.send('Hello from homepage.');
+    res.send(hometext);
 });
 
 app.listen(PORT, () => console.log(`Server running on Port: http://localhost:${PORT}`));
